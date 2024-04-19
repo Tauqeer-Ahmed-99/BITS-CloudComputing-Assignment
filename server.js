735d255db79e37bd57987730e956ce5e89dae976
Tanvir04khan <tauqueerrkhan@gmail.com>
2024-04-20 01:56:49 +0530

    import type

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-import express from "express";
-import dotenv from "dotenv";
-import bodyParser from "body-parser";
+// import express from "express";
+// import dotenv from "dotenv";
+// import bodyParser from "body-parser";
 
-import routeProtector from "./routes/routeProtector.js";
-import pageResolverRouter from "./routes/pageResolver.js";
+// import routeProtector from "./routes/routeProtector.js";
+// import pageResolverRouter from "./routes/pageResolver.js";
+
+const express = require("express");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+
+const routeProtector = require("./routes/routeProtector");
+const pageResolverRouter = require("./routes/pageResolver");
 
 dotenv.config();
 
